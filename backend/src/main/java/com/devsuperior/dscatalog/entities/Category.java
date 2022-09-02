0ceb86355ff386bcf4e73d5555e6ae4814a82f26
RodrigoWorst <rodrigobworst@live.com>
2022-09-01 22:11:54 -0300

    ORM mapping, database seeding

--- backend/src/main/java/com/devsuperior/dscatalog/entities/Category.java
+++ backend/src/main/java/com/devsuperior/dscatalog/entities/Category.java
@@ -2,36 +2,42 @@ package com.devsuperior.dscatalog.entities;
 
 import java.io.Serializable;
 import java.time.Instant;
+import java.util.HashSet;
 import java.util.Objects;
+import java.util.Set;
 
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToMany;
 import javax.persistence.PrePersist;
 import javax.persistence.PreUpdate;
 import javax.persistence.Table;
 
 @Entity
 @Table(name = "tb_category")
-public class Category implements Serializable{
-	
+public class Category implements Serializable {
+
 	private static final long serialVersionUID = 1L;
-	
+
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
 	private String name;
-	
+
 	@Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
 	private Instant createdAt;
-	
+
 	@Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
 	private Instant updatedAt;
-	
+
+	@ManyToMany(mappedBy = "categories")
+	private Set<Product> products = new HashSet<>();
+
 	public Category() {
-		
+
 	}
 
 	public Category(Long id, String name) {
@@ -58,21 +64,25 @@ public class Category implements Serializable{
 	public Instant getCreatedAt() {
 		return createdAt;
 	}
-	
+
 	public Instant getUpdatedAt() {
 		return updatedAt;
 	}
 
 	@PrePersist
 	public void prePersist() {
-		createdAt = Instant.now()
-;	}
-	
+		createdAt = Instant.now();
+	}
+
 	@PreUpdate
 	public void preUpdate() {
-		updatedAt = Instant.now()
-;	}
-	
+		updatedAt = Instant.now();
+	}
+
+	public Set<Product> getProducts() {
+		return products;
+	}
+
 	@Override
 	public int hashCode() {
 		return Objects.hash(id);
@@ -89,5 +99,5 @@ public class Category implements Serializable{
 		Category other = (Category) obj;
 		return Objects.equals(id, other.id);
 	}
-	
+
 }
---
+++ backend/src/test/java/com/devsuperior/dscatalog/tests/Factory.java
@@ -0,0 +1,26 @@
+package com.devsuperior.dscatalog.tests;
+
+import java.time.Instant;
+
+import com.devsuperior.dscatalog.dto.ProductDTO;
+import com.devsuperior.dscatalog.entities.Category;
+import com.devsuperior.dscatalog.entities.Product;
+
+public class Factory {
+	
+	public static Product createProduct() {
+		Product product = new Product(1L, "Phone", "Good Phone", 4000.0, "https://raw.githubusercontent.com/devsuperior/dscatalog-resources/master/backend/img/25-big.jpg", Instant.parse("2020-07-14T10:00:00Z"));
+		product.getCategories().add(new Category(2L, "Electronics"));
+		return product;
+	}
+	
+	public static ProductDTO createProductDTO() {
+		Product product = createProduct();
+		return new ProductDTO(product, product.getCategories());
+	}
+	
+	public static Category createCategory() {
+		return new Category(2L, "Electronics");
+	}
+
+}
