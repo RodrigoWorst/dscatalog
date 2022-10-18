299a81501f507ba3da0fe2126bb8c2d6fb843bc3
RodrigoWorst <rodrigobworst@live.com>
2022-10-18 16:51:57 -0300

    Page Catalog, Components ProductCard and ProductPrice

--- frontweb/src/App.tsx
+++ frontweb/src/App.tsx
@@ -1,10 +1,10 @@
-import Home from 'pages/Home';
+import Catalog from 'pages/Catalog';
 import './assets/styles/custom.scss';
 import './App.css';
 
 function App() {
   return (
-    <Home />
+    <Catalog />
   );
 }
 
