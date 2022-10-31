5e93c360f834f9257cd3adbe5c517baf30f38719
RodrigoWorst <rodrigobworst@live.com>
2022-10-31 10:19:52 -0300

    Saving auth data to localStorage

--- frontweb/src/util/request.ts
+++ frontweb/src/util/request.ts
@@ -1,27 +1,52 @@
 import axios from 'axios';
 import qs from 'qs';
 
-export const BASE_URL = process.env.REACT_APP_BACKEND_URL ?? 'http://localhost:8080';
+type Loginresponse = {
+  access_token: string;
+  token_type: string;
+  expires_in: number;
+  scope: string;
+  userFirstName: string;
+  userId: number;
+};
 
+export const BASE_URL =
+  process.env.REACT_APP_BACKEND_URL ?? 'http://localhost:8080';
+
+  const tokenkey = 'authData';
 const CLIENT_ID = process.env.REACT_APP_CLIENT_ID ?? 'dscatalog';
 const CLIENT_SECRET = process.env.REACT_APP_CLIENT_SECRET ?? 'dscatalog123';
 
 type LoginData = {
-    username: string;
-    password: string;
+  username: string;
+  password: string;
+};
+
+export const requestBackendLogin = (loginData: LoginData) => {
+  const headers = {
+    'Content-Type': 'application/x-www-form-urlencoded',
+    Authorization: 'Basic ' + window.btoa(CLIENT_ID + ':' + CLIENT_SECRET),
+  };
+
+  const data = qs.stringify({
+    ...loginData,
+    grant_type: 'password',
+  });
+
+  return axios({
+    method: 'POST',
+    baseURL: BASE_URL,
+    url: '/oauth/token',
+    data,
+    headers,
+  });
+};
+
+export const saveAuthData = (obj: Loginresponse) => {
+    localStorage.setItem(tokenkey, JSON.stringify(obj));
 }
 
-export const requestBackendLogin = (loginData : LoginData) => {
-
-    const headers = {
-        'Content-Type':'application/x-www-form-urlencoded',
-        Authorization: 'Basic ' + window.btoa(CLIENT_ID + ':' + CLIENT_SECRET)
-    }
-
-    const data = qs.stringify({
-        ...loginData,
-        grant_type: 'password'
-    })
-
-    return axios({method: 'POST', baseURL: BASE_URL, url: '/oauth/token', data, headers});
+export const getAuthData = () => {
+    const str = localStorage.getItem(tokenkey) ?? "{}";
+    return JSON.parse(str) as Loginresponse;
 }
