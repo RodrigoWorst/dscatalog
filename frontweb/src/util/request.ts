ff1e93f6e01e754a2373fdca88ae435ca3195d62
RodrigoWorst <rodrigobworst@live.com>
2022-10-31 09:50:08 -0300

    Form validation

--- frontweb/src/util/request.ts
+++ frontweb/src/util/request.ts
@@ -4,7 +4,7 @@ import qs from 'qs';
 export const BASE_URL = process.env.REACT_APP_BACKEND_URL ?? 'http://localhost:8080';
 
 const CLIENT_ID = process.env.REACT_APP_CLIENT_ID ?? 'dscatalog';
-const CLIEN_SECRET = process.env.REACT_APP_CLIENT_SECRET ?? 'dscatalog123';
+const CLIENT_SECRET = process.env.REACT_APP_CLIENT_SECRET ?? 'dscatalog123';
 
 type LoginData = {
     username: string;
@@ -15,7 +15,7 @@ export const requestBackendLogin = (loginData : LoginData) => {
 
     const headers = {
         'Content-Type':'application/x-www-form-urlencoded',
-        Authorization: 'Basic ' + window.btoa(CLIENT_ID + ':' + CLIEN_SECRET)
+        Authorization: 'Basic ' + window.btoa(CLIENT_ID + ':' + CLIENT_SECRET)
     }
 
     const data = qs.stringify({
